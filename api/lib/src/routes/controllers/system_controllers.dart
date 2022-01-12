cac56c42ccf3f601e8a1c8fd1d3b968dd922f371
debuggerx <dx8917312@163.com>
2022-01-12 18:33:10 +0800

    wip: get app version from db.

--- api/lib/src/routes/controllers/system_controllers.dart
+++ api/lib/src/routes/controllers/system_controllers.dart
@@ -3,24 +3,18 @@ import 'dart:async';
 import 'package:angel3_framework/angel3_framework.dart';
 import 'package:dde_gesture_manager_api/apis.dart';
 import 'package:dde_gesture_manager_api/src/models/app_version.dart';
-import 'package:file/file.dart';
-import 'package:yaml/yaml.dart';
-
-late FileSystem fs;
+import 'controller_extensions.dart';
 
 Future configureServer(Angel app) async {
   app.get(
     Apis.system.appVersion,
     (req, res) async {
-      var pubspec = fs.currentDirectory.parent.childDirectory('app').childFile('pubspec.yaml').readAsStringSync();
-      var version = loadYaml(pubspec)['version'] as String;
-      var versions = version.split('+');
-      return res.json(AppVersion(versionName: versions.first, versionCode: int.parse(versions.last)));
+      var appVersionQuery = AppVersionQuery();
+      appVersionQuery.orderBy(AppVersionFields.versionCode, descending: true);
+      return appVersionQuery.getOne(req.queryExecutor).then((value) => AppVersionResp(
+            versionName: value.value.versionName,
+            versionCode: value.value.versionCode,
+          ));
     },
   );
-}
-
-configureServerWithFileSystem(FileSystem fileSystem) {
-  fs = fileSystem;
-  return configureServer;
 }
